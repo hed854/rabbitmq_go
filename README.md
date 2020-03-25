fdaecc8a850f47fa684975fbfd7c4074f94bcc7d
hed854 <hed854k@gmail.com>
2020-03-25 23:31:00 +0100

    First Commit
    Fix

---
+++ README.md
@@ -0,0 +1,29 @@
+# Rabbitmq Golang Producer/Consumer
+
+Test your Rabbitmq things with this
+
+Based on: 
+
+* https://github.com/rabbitmq/rabbitmq-tutorials/tree/master/go
+* https://www.rabbitmq.com/tutorials/tutorial-one-go.html
+
+##Install
+
+```
+go get "github.com/streadway/amqp"
+```
+
+##Run producer 
+
+Note: all parameters are mandatory for now, ie this works only for topics :D
+
+```
+go run producer.go "hello worlllld" exchange routing_key
+```
+
+##Run consumer
+
+```
+go run consumer.go queue
+```
+
---
+++ consumer.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	queue := os.Args[1]
+
+	msgs, err := ch.Consume(
+		queue,         // queue
+		"GO Consumer", // consumer
+		true,          // auto-ack
+		false,         // exclusive
+		false,         // no-local
+		false,         // no-wait
+		nil,           // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
